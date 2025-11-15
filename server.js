08aabfd3c12caee17a097cbc559d0594c92969d7
Ismail Ahmad <ismailahmad0505@gmail.com>
2025-11-16 01:09:46 +0500

    tetsing

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000;
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-    res.send("hello world");
+    res.status(200);
 });
 
 app.listen(port, () => {
