7c52e025158e5348e0eb189409868e6ea191e15a
Ismail Ahmad <ismailahmad0505@gmail.com>
2025-11-17 18:48:31 +0500

    setup proper databases

--- db.js
+++ db.js
@@ -11,15 +11,41 @@ const pool = new Pool({
 });
 
 (async() => {
-    let request = await pool.query(`
-        CREATE TABLE user_sessions (
-    id SERIAL PRIMARY KEY,
-    user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
-    session_id TEXT NOT NULL UNIQUE,
-    refresh_token_hash TEXT NOT NULL UNIQUE,
-    is_revoked BOOLEAN NOT NULL DEFAULT FALSE,
-    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
-    expires_at TIMESTAMP WITH TIME ZONE NOT NULL
-);`);
-console.log(request);
+    try{
+        const request = await pool.query(`
+          CREATE TABLE user_sessions(
+            session_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+            user_id UUID REFERENCES users(id) ON DELETE CASCADE,
+            refresh_token TEXT NOT NULL,
+            is_revoke BOOLEAN DEFAULT FALSE NOT NULL,
+            created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+            expired_at TIMESTAMPTZ
+          );
+        `);
+        console.log("successful table creation!");
+    } catch(err) {
+        console.log(`Error: ${err.message}`);
+    }
 })();
+
+// users
+// CREATE TABLE users(
+//   name TEXT NOT NULL,
+//   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+//   email TEXT NOT NULL,
+//   password TEXT NOT NULL,
+//   token_version INTEGER NOT NULL,
+//   role TEXT NOT NULL,
+//   access JSONB DEFAULT '[]'::JSONB
+// );
+
+
+// user_sessions
+// CREATE TABLE user_sessions(
+//             session_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+//             user_id UUID REFERENCES users(id) ON DELETE CASCADE,
+//             refresh_token TEXT NOT NULL,
+//             is_revoke BOOLEAN DEFAULT FALSE NOT NULL,
+//             created_at TIMEZONE DEFAULT CURRENT_TIMEZONE,
+//             expired_at TIMEZONE,
+//           );
